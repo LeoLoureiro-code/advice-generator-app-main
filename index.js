906d465b71779118e70ded97768788a6169ecac5
Leo Loureiro <leolou.90@gmail.com>
2022-02-23 18:37:51 -0700

    add fetch to retrieve advice/advice number and show it

---
+++ index.js
@@ -0,0 +1,17 @@
+const API_URL = 'https://api.adviceslip.com/advice';
+const newAdvice = document.getElementById('new-advice');
+const adviceNumber = document.getElementById('advice-number');
+const adviceText = document.getElementById('advice');
+
+const getAdvice = () =>{
+    // advice.innerHTML = "loading...";
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(data => {
+        const {advice, id} = data.slip;
+        adviceNumber.innerHTML = id;
+        adviceText.innerHTML = advice;
+    })
+}
+
+newAdvice.addEventListener('click', getAdvice);
